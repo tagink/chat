d85283bcc0e772085d369c9210203410beba0aca
Simas Godovan <simas@godovan.lt>
2025-03-16 23:53:38 +0800

    feat: add hello world chat structure
    Signed-off-by: Simas Godovan <simas@godovan.lt>

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('App loaded successfully.');
+document.addEventListener("DOMContentLoaded", () => {
+    const messageForm = document.getElementById("message_form");
+    const messageInput = document.getElementById("message");
+    const messagesContainer = document.querySelector(".messages");
+
+    messageForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const messageText = messageInput.value.trim();
+
+        if (!messageText.length) {
+            return;
+        }
+
+        const newMessage = document.createElement("p");
+        newMessage.classList.add("mine");
+
+        const picture = document.createElement("span");
+        picture.classList.add("picture");
+        picture.textContent = "M";
+
+        const body = document.createElement("span");
+        body.classList.add("body");
+
+        const tag = document.createElement("span");
+        tag.classList.add("tag");
+        tag.textContent = "@mine";
+        const messageContent = document.createElement("span");
+        messageContent.classList.add("message");
+        messageContent.textContent = messageText;
+        body.appendChild(tag);
+        body.appendChild(messageContent);
+        newMessage.appendChild(picture);
+        newMessage.appendChild(body);
+        messagesContainer.appendChild(newMessage);
+
+        messagesContainer.scrollIntoView({ behavior: "smooth", block: "end", inline: "end" });
+        messageInput.value = "";
+    });
 });
